baa296d277b2f40099e3a7f4c712426c7c087411
Dominik Winecki <dominikwinecki@gmail.com>
2017-04-03 15:27:14 -0700

    Refactor PowerAnalyser and add documentation

--- src/org/usfirst/frc/team1014/robot/PowerAnalyser.java
+++ src/org/usfirst/frc/team1014/robot/PowerAnalyser.java
@@ -10,11 +10,22 @@ import java.util.Optional;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+/**
+ * This class logs the current draw of individual channels on the PDP. It logs
+ * to {@literal /home/lvuser/powerlog-<UNIX TIMESTAMP>.csv}.
+ */
 public class PowerAnalyser {
 	PowerDistributionPanel pdp;
-	LinkedHashMap<String, Integer> channels;
 	String fileName = "/home/lvuser/powerlog-" + System.currentTimeMillis() / 1000 + ".csv";
 	Optional<FileWriter> fileWriter = Optional.empty();
+
+	/*
+	 * The LinkedHashMap is used because it preserves order when used as an
+	 * iterator. This makes sure that every time you run through the channels it
+	 * prints to the file in the correct order and it matches the order that
+	 * each channel was added to the PowerAnalyser.
+	 */
+	LinkedHashMap<String, Integer> channels;
 
 	public PowerAnalyser(PowerDistributionPanel pdp) {
 		channels = new LinkedHashMap<>();
@@ -28,44 +39,64 @@ public class PowerAnalyser {
 
 	}
 
+	/**
+	 * Adds a new channel with a name and channel number (from PDP).
+	 * 
+	 * @param name
+	 * @param channel
+	 */
 	public void add(String name, int channel) {
 		channels.put(name + "-current", channel);
 	}
 
-	public void update() {
-		channels.forEach((k, v) -> SmartDashboard.putDouble(k, pdp.getCurrent(v)));
+	/**
+	 * Prints the headers (names) of each column to the log file. This should
+	 * only be called after all the channels are added.
+	 */
+	public void printFieldHeader() {
 		if (!fileWriter.isPresent())
 			return;
-		List<String> valueStrings = new ArrayList<>();
-		channels.forEach((k, v) -> valueStrings.add(String.valueOf(pdp.getCurrent(v))));
-		String[] vals = new String[channels.size()];
-		valueStrings.toArray(vals);
+
+		String[] vals = getCurrentValuesAsStrings();
+		appendStrings(vals);
+	}
+
+	/**
+	 * Prints a new line to the log and {@link SmartDashboard}.
+	 */
+	public void update() {
+		// Writes current data to SmartDashboard
+		channels.forEach((k, v) -> SmartDashboard.putDouble(k, pdp.getCurrent(v)));
+
+		if (!fileWriter.isPresent())
+			return;
+
+		String[] vals = getCurrentValuesAsStrings();
+		appendStrings(vals);
+	}
+
+	/**
+	 * Writes the values to the log.
+	 */
+	private void appendStrings(String[] vals) {
 		try {
 			fileWriter.get().write(String.join(", ", vals) + "\n");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		try {
 			fileWriter.get().flush();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	public void printFieldHeader() {
-		if (!fileWriter.isPresent())
-			return;
+	/**
+	 * @return an array of Strings representing the current of each channel, in
+	 *         the order used by the channels list
+	 */
+	private String[] getCurrentValuesAsStrings() {
 		List<String> valueStrings = new ArrayList<>();
 		channels.forEach((k, v) -> valueStrings.add(k));
 		String[] vals = new String[channels.size()];
 		valueStrings.toArray(vals);
-		try {
-			fileWriter.get().write(String.join(", ", vals) + "\n");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		return vals;
 	}
 
 }
