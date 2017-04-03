dominikWin/BadRobot2017

commit c0a2864f87081efca0eb4d726d9bc7273d7a1b18

Working code from practice day of Regional

src/org/usfirst/frc/team1014/robot/utils/SwerveWheel.java
@@ -31,7 +31,7 @@ public class SwerveWheel {
 		this.encoderMin = encoderMin;
 		range = encoderMax - encoderMin;
 
-		perpendicular = location.perpendicularCW();
+		perpendicular = location;
 		perpendicular = perpendicular.normalize();
 
 		drive = new CANTalon(driveMotorPin);
@@ -39,7 +39,7 @@ public class SwerveWheel {
 
 		pivot.changeControlMode(TalonControlMode.Position);
 		pivot.setFeedbackDevice(FeedbackDevice.AnalogEncoder);
-		pivot.setPID(32, 0, 0);
+		pivot.setPID(16, 0, 0);
 		pivot.enableControl();
 
 	}
@@ -53,8 +53,13 @@ public class SwerveWheel {
 
 		double currentPosition = pivot.getPosition();
 		double rawCurrent = pivot.getAnalogInRaw();
+		
+		
+		if(id.equals("D")){
+			System.out.println(id + ": " + pivot.getAnalogInRaw());
+		}else
+			System.out.print(id + ": " + pivot.getAnalogInRaw() + "  ");
 
-		System.out.println(id + ": " + rawCurrent);
 
 		double currentRadians = (Math.PI * 2.0 * (rawCurrent - offset)) / range;
 
@@ -83,6 +88,13 @@ public class SwerveWheel {
 		pivot.set(finalPosition);
 
 		normalizer.add(drive, speed);
+	}
+	
+	public void print(){
+		if(id.equals("D")){
+			System.out.println(id + ": " + pivot.getAnalogInRaw());
+		}else
+			System.out.print(id + ": " + pivot.getAnalogInRaw() + "  ");
 	}
 
 	/**

src/org/usfirst/frc/team1014/robot/Robot.java
@@ -20,10 +20,11 @@ import org.usfirst.frc.team1014.robot.commands.auto.ShootRight;
 import org.usfirst.frc.team1014.robot.subsystems.LEDLights.LEDState;
 import org.usfirst.frc.team1014.robot.utils.Vector2d;
 
+import com.ctre.CANTalon;
+
 import edu.wpi.cscore.UsbCamera;
-import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -46,6 +47,8 @@ public class Robot extends IterativeRobot {
 	UsbCamera camera;
 
 	public static OI oi;
+	
+	CANTalon climber;
 
 	/**
 	 * This function is run when the robot is first started up and should be
@@ -78,9 +81,11 @@ public class Robot extends IterativeRobot {
 		smartDashboard.putNumber("Delay", 0);
 		smartDashboard.putData("Drive Mode Chooser", driveChooser);
 		smartDashboard.putData("Auto Chooser", autoChooser);
+		
+		climber = new CANTalon(30);
 
-		camera = CameraServer.getInstance().startAutomaticCapture();
-		camera.setResolution(640, 480);
+		//camera = CameraServer.getInstance().startAutomaticCapture();
+		//camera.setResolution(640, 480);
 		
 		if (CommandBase.lights != null) {
 			CommandBase.lights.setLights(LEDState.kDEFAULT);
@@ -98,7 +103,7 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void teleopInit() {
 		stateChangeInit();
-		teleopGroup.addParallel((Command) driveChooser.getSelected());
+		//teleopGroup.addParallel((Command) driveChooser.getSelected());
 		Scheduler.getInstance().add(teleopGroup);
 	}
 
@@ -107,9 +112,7 @@ public class Robot extends IterativeRobot {
 		stateChangeInit();
 		//autoGroup.addSequential(new AutoDelay(smartDashboard.getNumber("Delay", 0)));
 		//autoGroup.addSequential((Command) autoChooser.getSelected());
-		
-		autoGroup.addSequential(new AutoDrive(.5, new Vector2d(0,1)));
-		autoGroup.addSequential(new AutoDrive(1, new Vector2d(0, -1)));
+		autoGroup.addSequential(new AutoDrive(2, new Vector2d(0,1)));
 		Scheduler.getInstance().add(autoGroup);
 	}
 
@@ -136,7 +139,7 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void teleopPeriodic() {
 		periodic();
-		//climbTalon.set(oi.xboxController0.getTriggerAxis(Hand.kLeft) - oi.xboxController0.getTriggerAxis(Hand.kRight));
+		climber.set(oi.xboxController1.getTriggerAxis(Hand.kLeft) - oi.xboxController1.getTriggerAxis(Hand.kRight));
 	}
 
 	@Override

src/org/usfirst/frc/team1014/robot/commands/Print.java
@@ -1,0 +1,18 @@
+package org.usfirst.frc.team1014.robot.commands;
+
+import org.usfirst.frc.team1014.robot.subsystems.DriveTrain;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class Print extends Command{
+
+	protected void execute(){
+		DriveTrain.getInstance().test();
+	}
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}

src/org/usfirst/frc/team1014/robot/commands/TeleDrive.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team1014.robot.commands;
 
 import org.usfirst.frc.team1014.robot.OI;
+import org.usfirst.frc.team1014.robot.commands.auto.AutoRotate;
 import org.usfirst.frc.team1014.robot.subsystems.DriveTrain;
 import org.usfirst.frc.team1014.robot.utils.Vector2d;
 
@@ -8,57 +9,69 @@ import edu.wpi.first.wpilibj.command.Command;
 
 public class TeleDrive extends Command {
 
-	/*private static boolean isSwerve;
-	private static boolean isAbsolute;
-	private static double badEncoderPosition;
-	private static boolean Xbutton, Abutton;
-
-	protected void initialize() {
-
-		isSwerve = true;
-		isAbsolute = true;
-		Xbutton = false;
-		Abutton = false;
-
-	}*/
-
+	/*
+	 * private static boolean isSwerve; private static boolean isAbsolute;
+	 * private static double badEncoderPosition; private static boolean Xbutton,
+	 * Abutton;
+	 * 
+	 * protected void initialize() {
+	 * 
+	 * isSwerve = true; isAbsolute = true; Xbutton = false; Abutton = false;
+	 * 
+	 * }
+	 */
+	private static boolean AButton;
+	
+	protected void initialize(){
+		AButton = false;
+	}
+	
+	double angle;
+	AutoRotate rotate;
+	
 	public void execute() {
 		double rotation = OI.xboxController0.getRawAxis(4);
 		if (Math.abs(rotation) < .15)
 			rotation = 0;
+
+		DriveTrain.getInstance().drive(rotation,
+				new Vector2d(OI.xboxController0.getRawAxis(0), -OI.xboxController0.getRawAxis(1)));
 		
-		DriveTrain.getInstance().drive(-rotation,
-				new Vector2d(-OI.xboxController0.getRawAxis(0), OI.xboxController0.getRawAxis(1)));
-
-		/*if (OI.xboxController0.getRawButton(3)) {
-			Xbutton = true;
-		} else if (!OI.xboxController0.getRawButton(3) && Xbutton) {
-			Xbutton = false;
-			isSwerve = !isSwerve;
+		if(OI.xboxController0.getAButton()){
+			AButton = true;
+		}
+		if(OI.xboxController0.getAButton() && AButton){
+			DriveTrain.getInstance().zeroYaw();
+			AButton = false;
 		}
 
-		if (OI.xboxController0.getRawButton(1)) {
-			Abutton = true;
-		} else if (!OI.xboxController0.getRawButton(1) && Abutton) {
-			Abutton = false;
-			isAbsolute = !isAbsolute;
-			if (isAbsolute == false)
-				DriveTrain.getInstance().setRelative();
-		}
-
-		if (isSwerve && isAbsolute) {
-			
-		}
-
-		if (isSwerve && !isAbsolute) {
-			DriveTrain.getInstance().relativeDrive(rotation,
-					new Vector2d(OI.xboxController0.getRawAxis(0), -OI.xboxController0.getRawAxis(1)));
-		}
-
-		if (!isSwerve) {
-			DriveTrain.getInstance().tankDrive(-OI.xboxController0.getRawAxis(5), -OI.xboxController0.getRawAxis(1),
-					badEncoderPosition);
+		/*angle = OI.xboxController0.getPOV();
+		
+		if (OI.xboxController0.getPOV() != -1) {
+			angle = 360 - angle;
+			rotate = new AutoRotate(angle);
+			rotate.start();
 		}*/
+		/*
+		 * 
+		 * if (OI.xboxController0.getRawButton(1)) { Abutton = true; } else if
+		 * (!OI.xboxController0.getRawButton(1) && Abutton) { Abutton = false;
+		 * isAbsolute = !isAbsolute; if (isAbsolute == false)
+		 * DriveTrain.getInstance().setRelative(); }
+		 * 
+		 * if (isSwerve && isAbsolute) {
+		 * 
+		 * }
+		 * 
+		 * if (isSwerve && !isAbsolute) {
+		 * DriveTrain.getInstance().relativeDrive(rotation, new
+		 * Vector2d(OI.xboxController0.getRawAxis(0),
+		 * -OI.xboxController0.getRawAxis(1))); }
+		 * 
+		 * if (!isSwerve) {
+		 * DriveTrain.getInstance().tankDrive(-OI.xboxController0.getRawAxis(5),
+		 * -OI.xboxController0.getRawAxis(1), badEncoderPosition); }
+		 */
 
 	}
 

src/org/usfirst/frc/team1014/robot/commands/TeleopGroup.java
@@ -1,12 +1,15 @@
 package org.usfirst.frc.team1014.robot.commands;
 
+import org.usfirst.frc.team1014.robot.OI;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class TeleopGroup extends CommandGroup {
 	
 	public TeleopGroup() {
+		this.addParallel(new TeleDrive());
 		this.addParallel(new UseShooter());
-		this.addParallel(new UseClimber());
+		//this.addParallel(new UseClimber());
 	}
 
 }

src/org/usfirst/frc/team1014/robot/commands/UseClimber.java
@@ -14,10 +14,7 @@ public class UseClimber extends Command {
 	}
 
 	protected void execute() {
-		if (OI.xboxController0.getRawAxis(3) > 0)
-			climber.climb(OI.xboxController0.getRawAxis(3));
-		else if (OI.xboxController0.getRawAxis(2) > 0)
-			climber.climb(-OI.xboxController0.getRawAxis(2));
+		climber.climb(OI.xboxController1.getRawAxis(5));
 	}
 
 	@Override

src/org/usfirst/frc/team1014/robot/commands/UseShooter.java
@@ -14,13 +14,15 @@ public class UseShooter extends Command {
 	}
 
 	protected void execute() {
-		if (Math.abs(OI.xboxController1.getRawAxis(5)) > .15) {
+		if (Math.abs(OI.xboxController1.getRawAxis(5)) > .2) {
 			shooter.shoot(-1);
-		}
-		shooter.rotateFeeder(-OI.xboxController1.getRawAxis(1));
-		
-		System.out.println(-OI.xboxController1.getRawAxis(1));
+			shooter.rotateFeeder(-OI.xboxController1.getRawAxis(5));
 
+		} else {
+			shooter.shoot(0);
+			shooter.rotateFeeder(0);
+	
+		}
 	}
 
 	@Override
@@ -30,4 +32,3 @@ public class UseShooter extends Command {
 	}
 
 }
-

src/org/usfirst/frc/team1014/robot/commands/auto/AutoDrive.java
@@ -20,7 +20,7 @@ public class AutoDrive extends Command {
 	 */
 	public AutoDrive(double time, Vector2d translation) {
 		this.time = time * 1000000;
-		this.translation = new Vector2d(translation.getX(), -translation.getY());
+		this.translation = new Vector2d(translation.getX(), translation.getY());
 		startTime = Utility.getFPGATime();
 	}
 

commit e4deb1665431b4e6bef4f9bc8983545dbd9b21f8

Fix perpendicular rotation vector code
This commit sets each wheels perpendicular variable to the perpendicular of its
location. Without this change the robot would turn by facing each module tword
the center of the robot and driving each motor against another.

This fixes the bug introduced in c0a2864f87081efca0eb4d726d9bc7273d7a1b18

src/org/usfirst/frc/team1014/robot/utils/SwerveWheel.java
@@ -31,7 +31,7 @@ public class SwerveWheel {
 		this.encoderMin = encoderMin;
 		range = encoderMax - encoderMin;
 
-		perpendicular = location;
+		perpendicular = location.perpendicularCW();
 		perpendicular = perpendicular.normalize();
 
 		drive = new CANTalon(driveMotorPin);